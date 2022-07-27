b53253a77c5b7bafa33505a8cd053c44e46b16d6
klucas33 <klucas96@naver.com>
2022-07-28 00:03:27 +0900

    finally1

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
-import {BrowserRouter as Router, Switch, Route} from "react-router-dom";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Detail from "./routes/Detail";
 import Home from "./routes/Home";
 
 function App() {
-  return <Router>
-            <Switch>
-              <Route path="/movie/:id">
-                <Detail />
-              </Route>
-              <Route path="/">
-              <Home />
-              </Route>
-            </Switch>
-        </Router>;
+  return (
+    <Router basename="https://klucas33.github.io/react-for-beginners">
+      <Switch>
+        <Route path="/movie/:id">
+          <Detail />
+        </Route>
+        <Route path="/">
+          <Home />
+        </Route>
+      </Switch>
+    </Router>
+  );
 }
 
 export default App;
